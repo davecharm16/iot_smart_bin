14bcfd5512f97a39bd7d4e887249d93a1bdd3a5d
Dave Charm Bulaquena <i_davecharm.bulaquena@stratpoint.com>
2024-05-16 09:53:02 +0800

    Inital Commit

---
+++ iot_smart_bin.ino
@@ -0,0 +1,56 @@
+#include <ESP32Servo.h>
+#include <WiFi.h>
+
+#define WIFI_SSID "HUAWEI-W8wQ"
+#define WIFI_PASSWORD "wonderful"
+#define FIREBASE_HOST "fir-app-example.firebaseio.com"
+#define FIREBASE_AUTH "examplesd2asdasdasdasd2asd3asd2asd2as32das3d2as2da3"
+
+
+Servo myservo;  // create servo object to control a servo
+// 16 servo objects can be created on the ESP32
+ 
+int pos = 0;    // variable to store the servo position
+// Recommended PWM GPIO pins on the ESP32 include 2,4,12-19,21-23,25-27,32-33 
+int servoPin = 13;
+
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+}
+ 
+void setup() {
+	// Allow allocation of all timers
+	ESP32PWM::allocateTimer(0);
+	ESP32PWM::allocateTimer(1);
+	ESP32PWM::allocateTimer(2);
+	ESP32PWM::allocateTimer(3);
+	myservo.setPeriodHertz(50);    // standard 50 hz servo
+	myservo.attach(servoPin, 500, 2400); 
+  
+  initWiFi();
+  Serial.print("RRSI: ");
+  Serial.println(WiFi.RSSI());
+  Serial.println("Setup done");
+}
+
+
+ 
+void loop() {
+ 
+	for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+		// in steps of 1 degree
+		myservo.write(pos);    // tell servo to go to position in variable 'pos'
+		delay(15);             // waits 15ms for the servo to reach the position
+	}
+	for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+		myservo.write(pos);    // tell servo to go to position in variable 'pos'
+		delay(15);             // waits 15ms for the servo to reach the position
+	}
+}
